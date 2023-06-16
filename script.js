c84041c1d0cb6c988cc183c35b80c4932575ba3d
rafael <rafadesuyobusiness@gmail.com>
2023-06-16 15:53:37 -0300

    updated js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
 //Down bellow are the number of each lamp, lamps are divided by section name.
 let whatIsBinaryLamp = 0
+const light = document.getElementById('light-example-1')
+const currentColor = light.getAttribute('fill')
+const number0 = document.getElementById('numberToggle0')
 
 //Todo add other lamps down here
 
@@ -9,32 +12,23 @@ let whatIsBinaryLamp = 0
 const clickSound = new Audio("assets/click.mp3"); 
 
 function toggleLight() {
-    const light = document.getElementById('light-example-1')
     const currentColor = light.getAttribute('fill')
     clickSound.play()
     if (currentColor === '#0C1016') {
-        light.setAttribute('fill', '#FFD542')
-    } else {
-        light.setAttribute('fill', '#0C1016')
+        light.setAttribute('fill', '#FFD542');
+    }else{
+        light.setAttribute('fill', '#0C1016');
     }
-
-    //switching the numbers
-    const number0 = document.getElementById('numberToggle0')
-    const number1 = document.getElementById('numberToggle1')
     
     if(whatIsBinaryLamp == 0)
     {
-        //display none = invisible
-        //display block = visible
-        number0.style.display = "none"
-        number1.style.display = "block"
-        whatIsBinaryLamp = 1
+        number0.textContent = "1";
+        whatIsBinaryLamp = 1;
     }
     else if(whatIsBinaryLamp == 1)
     {
-        number1.style.display = "none"
-        number0.style.display = "block"
-        whatIsBinaryLamp = 0
+        number0.textContent = "0";
+        whatIsBinaryLamp = 0;
     }
     
 }
